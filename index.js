22e331ef5979411bb8591377956ff44d8e696d68
Emanuele Meregalli <c.meregalliemanuele@itis-molinari.eu>
2024-10-28 12:19:44 +0100

    creazione struttura di base

---
+++ index.js
@@ -0,0 +1,5 @@
+import {createTable} from './table.js';
+import {createForm} from './form.js';
+const table = createTable(document.querySelector("#table1"));
+const form = createForm(document.querySelector('#app'));
+
