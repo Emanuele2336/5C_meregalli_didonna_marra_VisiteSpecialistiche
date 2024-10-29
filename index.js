b2b336ade860106e73499bc8232d8294a92639bd
unknown <dilorenzo@itis-molinari.eu>
2024-10-29 10:34:15 +0100

    fetch json bottoni

--- index.js
+++ index.js
@@ -6,29 +6,30 @@ const table = createTable(document.querySelector("#table1"));
 /*table.build([["Cognome", "Voto"], ["Pogba", "6"], ["Vlahovic", 8], ["Thuram", 6.5]]);
 table.render();
 */
-
-let but = createButton(document.querySelector("#div"));
-but.setLabel("ciao");
-but.render();
+const buttons=[];
 let tipologieArray = [];
-fetch('conf.json')
+
+  fetch("conf.json")
   .then(response => response.json())  
   .then(data => {
     tipologieArray = tipologieArray.concat(data.tipologie);
-    console.log(tipologieArray); // Stampa l'array aggiornato
-  })
-  .catch(error => console.error('Errore nel caricamento dei dati:', error));
+    for(let i =0;i<tipologieArray.length;i++){
+      let b = createButton(document.querySelector("#bottoni"));
+      b.setLabel(tipologieArray[i]);
+      buttons.push(b);
+      b.render();
+    }
+  
+})
 
-    //document.getElementById("tabellacardiologia").style.visibility = "hidden";
-    //document.getElementById("tabellacardiologia").style.visibility = "hidden";
-    //document.getElementById("tabellacardiologia").style.visibility = "hidden";
-    //document.getElementById("tabellacardiologia").style.visibility = "hidden";
+
+/*
 let bot1 =createButton(document.querySelector("#div"));
 let bot2 =createButton(document.querySelector("#div"));
 let bot3 =createButton(document.querySelector("#div"));
 let bot4 =createButton(document.querySelector("#div"));
 let bot5 =createButton(document.querySelector("#div"));
-
+console.log(tipologieArray);
 bot1.setLabel(tipologieArray[0]);
 bot2.setLabel(tipologieArray[1]);
 bot3.setLabel(tipologieArray[2]);
@@ -39,9 +40,9 @@ bot1.render();
 bot2.render();
 bot3.render();
 bot4.render();
-bot5.render();
+bot5.render();*/
 
-
+console.log( document.querySelector("#buttonCardiologia"))
 
 
 
