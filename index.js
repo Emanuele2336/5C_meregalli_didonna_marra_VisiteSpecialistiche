a231cbf1317de694d1b8659e91cb1ca6ff21e852
Emanuele Meregalli <c.meregalliemanuele@itis-molinari.eu>
2024-10-29 09:33:35 +0100

    iii

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 //import {createTable} from './table.js';
 //import {createForm} from './form.js';
-//const table = createTable(document.querySelector("#table1"));
+const table = createTable(document.querySelector("#table1"));
 //const form = createForm(document.querySelector('#app'));
 
-/*table.build([["Cognome", "Voto"], ["Pogba", "6"], ["Vlahovic", 8], ["Thuram", 6.5]]);
+table.build([["lunedì", "martedì","mercoledì","giovedì","venerdì"], ["Pogba", "6"], ["Vlahovic", 8], ["Thuram", 6.5]]);
 table.render();
-*/
+
 
 
 
 const bottoneCardiologia=document.getElementById("bottCardiologia");
 bottoneCardiologia.onclick=()=>{
-    console.log("ciao");
+    document.getElementById("tabellacardiologia").style.visibility = "hidden";
+    document.getElementById("tabellacardiologia").style.visibility = "hidden";
+    document.getElementById("tabellacardiologia").style.visibility = "hidden";
+    document.getElementById("tabellacardiologia").style.visibility = "hidden";
 }
-bottoneCardiologia.onclick=()=>{
-    
-}
+
 
 
 
